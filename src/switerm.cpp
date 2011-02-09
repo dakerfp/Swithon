f6cf08e89284ef4db317eba994c49682c2a1ce4f
Daker Fernandes Pinheiro <daker.pinheiro@openbossa.org>
2011-02-08 21:27:33 -0300

    Started to create conversion from term_t to SwiTerm.
    Signed-off-by: Daker Fernandes Pinheiro <daker.pinheiro@openbossa.org>

--- src/switerm.cpp
+++ src/switerm.cpp
@@ -60,8 +60,41 @@ SwiTerm *SwiTerm::create(const char*, const std::list<SwiTerm*> &)
     return 0; // TODO
 }
 
-SwiTerm *SwiTerm::create(const term_t &)
+SwiTerm *SwiTerm::create(const term_t &term)
 {
-    return 0; // TODO
+    switch( PL_term_type(term) ) {
+        // TODO add lists, strings and vars support
+    case PL_ATOM:
+
+    case PL_INTEGER:
+        long long_value;
+        PL_get_long(term, &long_value);
+        return new SwiInt(long_value);
+
+    case PL_FLOAT:
+        double float_value;
+        PL_get_float(term, &float_value);
+        return new SwiFloat(float_value);
+
+    case PL_TERM:
+        term_t atom_name;
+        int functor_arity;
+
+        PL_get_name_arity(term, &atom_name, &functor_arity);
+
+        SwiTerm *subterms[functor_arity];
+
+        for(int n = 1; n <= functor_arity; n++) {
+            term_t aux = PL_new_term_ref();
+            PL_get_arg(n, term, aux);
+            subterms[n-1] = SwiTerm::create(aux);
+        }
+
+        return new SwiFunctor(PL_atom_chars(atom_name), subterms, functor_arity);
+
+    default:
+        return 0; // TODO
+    }
+
 }
 
